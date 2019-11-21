d40436736dd965a860d4b4a20709ffe77bb98df4
Craig Storer <craigstorer@gmail.com>
2019-11-21 03:21:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,14 @@
 def unsafe?(speed)
-  if speed > 60 
-    puts true
-  elsif speed < 40 ? true : false
-
-end
+  speed > 60 ? true : false
+elsif
+  speed < 40 ? true : false
 end
 
 
 
 
 def not_safe?(speed)
-	speed
+	
 end
 	
 
